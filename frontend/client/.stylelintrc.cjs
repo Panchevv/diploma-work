78ff61e40ea9f90f27f0830011510a7906dd1e0a
Panchevv <92977748+Panchevv@users.noreply.github.com>
2024-04-28 18:48:22 +0300

    Frontend initial commit

---
+++ frontend/client/.stylelintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    'extends': [
+      "stylelint-config-standard-scss",
+      "stylelint-config-standard-vue/scss"
+    ],
+    'rules': {
+      'indentation': 4,
+      'color-no-hex': true,
+      'color-hex-length': null,
+    }
+  }
---
+++ frontend/client/vite.config.ts
@@ -0,0 +1,46 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
+import svgLoader from 'vite-svg-loader'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      script: {
+        defineModel: true
+      }
+    }),
+    vuetify({
+      styles: {
+        configFile: fileURLToPath(new URL('./src/assets/styles/settings.scss', import.meta.url))
+      }
+    }),
+    svgLoader(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  preview: {
+    port: 3000
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/graphql': {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        ws: true
+      },
+      '/keycloak.json': {
+        target: "http://localhost:8085",
+        changeOrigin: true,
+        ws: true
+      }
+    }
+  },
+})
